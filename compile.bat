bdcc82dfa2bf4a3cb4a71905b9138d510fc2a5e1
yonatan.eavri <yonatan.eavri@gmail.com>
2023-08-30 23:00:37 +0300

    Changed compile.bat to be more efficient with processes

--- compile.bat
+++ compile.bat
@@ -1,4 +1,1 @@
-mingw64.exe bison -o parser.cpp -d parser.y
-mingw64.exe flex -o lex.yy.cc lexer.l
-
-mingw64.exe g++ parser.cpp lex.yy.cc -o parser
+mingw64.exe sh -c "bison -o parser.cpp -d parser.y && flex -o lex.yy.cc lexer.l && g++ parser.cpp lex.yy.cc -o parser"
