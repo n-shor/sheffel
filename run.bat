b67e2a1e707393067f881ea9a18cc6d3f0bd0b13
yonatan.eavri <yonatan.eavri@gmail.com>
2023-11-08 18:29:40 +0200

    updated run.bat to fix issue when build directory doesn't exist

--- run.bat
+++ run.bat
@@ -1,10 +1,14 @@
-set "build_dir=x64"
-set "input_file=input.shf"
+set "build_dir=./x64"
+set "source_dir=."
+set "input_file=./input.shf"
 
-copy "./common.hpp" "%build_dir%/common.hpp"
-bison -o "%build_dir%/parser.cpp" -d "./parser.y"
-flex -o "%build_dir%/lex.yy.cc" "./lexer.l"
+
+if not exist "%build_dir%" mkdir "%build_dir%"
+
+copy "%source_dir%/common.hpp" "%build_dir%/common.hpp"
+bison -o "%build_dir%/parser.cpp" -d "%source_dir%/parser.y"
+flex -o "%build_dir%/lex.yy.cc" "%source_dir%/lexer.l"
 
 g++ "%build_dir%/parser.cpp" "%build_dir%/lex.yy.cc" -o "%build_dir%/parser" -static-libgcc -static-libstdc++ -static
 
-start "" ./%build_dir%/parser.exe %input_file%
+start "" %build_dir%/parser.exe %input_file%
