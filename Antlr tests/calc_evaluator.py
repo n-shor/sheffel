208d872d262b6118eeae84512f39e66bdd60f862
nadva <nbm1401@gmail.com>
2023-12-19 22:21:24 +0200

    Changed stack name to list in calc_evaluator

--- Antlr tests/calc_evaluator.py
+++ Antlr tests/calc_evaluator.py
@@ -5,59 +5,59 @@ from CalcListener import CalcListener
 
 class CalcEvaluator(CalcListener):
     def __init__(self):
-        self.stack = []
+        self.list = []
 
     def exitInt(self, ctx: CalcParser.IntContext):
-        self.stack.append(int(ctx.getText()))
+        self.list.append(int(ctx.getText()))
 
     def exitFloat(self, ctx: CalcParser.FloatContext):
-        self.stack.append(float(ctx.getText()))
+        self.list.append(float(ctx.getText()))
 
     def exitAddSub(self, ctx:CalcParser.AddSubContext):
         # Handle expressions with '+' and '-' operators
         for child in ctx.getChildren():
             if isinstance(child, CalcParser.MulDivContext):
-                # Extract and push term values onto the stack
-                value = self.stack.pop()
-                self.stack.append(value)
+                # Extract and push term values onto the list
+                value = self.list.pop()
+                self.list.append(value)
             elif isinstance(child, TerminalNode):
                 # Handle operators: +, -
                 op = child.getText()
-                right = self.stack.pop()
-                left = self.stack.pop()
+                right = self.list.pop()
+                left = self.list.pop()
                 if op == '+':
-                    self.stack.append(left + right)
+                    self.list.append(left + right)
                 elif op == '-':
-                    self.stack.append(left - right)
+                    self.list.append(left - right)
 
-        # Final value is at the top of the stack
-        self.stack.append(self.stack.pop())
+        # Final value is at the top of the list
+        self.list.append(self.list.pop())
 
     def exitMulDiv(self, ctx:CalcParser.MulDivContext):
         # Handle expressions with '*' and '/' operators
         for child in ctx.getChildren():
             if isinstance(child, CalcParser.FactorContext):
-                # Extract and push factor values onto the stack
-                value = self.stack.pop()
-                self.stack.append(value)
+                # Extract and push factor values onto the list
+                value = self.list.pop()
+                self.list.append(value)
             if isinstance(child, TerminalNode):
                 # Handle operators: *, /
                 op = child.getText()
-                right = self.stack.pop()
-                left = self.stack.pop()
+                right = self.list.pop()
+                left = self.list.pop()
                 if op == '*':
-                    self.stack.append(left * right)
+                    self.list.append(left * right)
                 elif op == '/':
-                    self.stack.append(left / right)
+                    self.list.append(left / right)
 
-        # Final value is at the top of the stack
-        self.stack.append(self.stack.pop())
+        # Final value is at the top of the list
+        self.list.append(self.list.pop())
 
     def exitFactor(self, ctx: CalcParser.FactorContext):
         if ctx.getChildCount() == 1:
-            self.stack.append(float(ctx.getText()))
+            self.list.append(float(ctx.getText()))
 
             
     def getValue(self):
-        # Get the final value from the stack
-        return self.stack.pop() if self.stack else None
+        # Get the final value from the list
+        return self.list.pop() if self.list else None
