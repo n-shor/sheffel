4d04e85644ced02437676a0840583c68269755be
nadva <nbm1401@gmail.com>
2023-12-26 19:56:21 +0200

    Changed the grammar file's name and its related variables

--- run.py
+++ run.py
@@ -2,11 +2,12 @@ from compiler.compile import *
 
 
 def main():
-    compile_file('./code.shf', alternative_ast=[
+    """compile_file('./code.shf', alternative_ast=[
         VariableDeclaration('x', ir.IntType(32)),
         BinaryOperator('=', Variable('x'), IntegralLiteral(13)),
         BinaryOperator('=', Variable('x'), BinaryOperator('+', BinaryOperator('-', Variable('x'), IntegralLiteral(3)), IntegralLiteral(8)))
-    ], make_executable=False)
+    ], make_executable=False)"""
+    #compile_file('./code.shf', make_executable=False, run_ir_generator=False)
 
 
 if __name__ == "__main__":
