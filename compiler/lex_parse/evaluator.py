36b127f272f23c3badb57eaf5c7368fe9cb810fb
yonatan.eavri <yonatan.eavri@gmail.com>
2024-01-11 21:37:32 +0200

    Fixed the behavior qualifier creation for variable declarations

--- compiler/lex_parse/evaluator.py
+++ compiler/lex_parse/evaluator.py
@@ -67,9 +67,8 @@ class GrammarASTBuilder(GrammarListener):
 
     def exitVar(self, ctx: GrammarParser.VarContext):
         print("variable")
-        # if ctx.parentCtx.get:
-        # discuss read/write var returning problems with eavri
         return Variable(ctx.getText())
+        # missing the read/write classification
 
     def exitAssignment(self, ctx: GrammarParser.AssignmentContext):
         print("assignment")
@@ -105,17 +104,15 @@ class GrammarASTBuilder(GrammarListener):
         print("declaration")
 
         *_, name = ctx.getChildren()
+        unqualified_type_name = ctx.getChild(0, GrammarParser.TypeContext).getText()
+        memory_qualifier_name = ctx.getChild(0, GrammarParser.MemoryQualifierContext).getText()
+        behavior_qualifier_names = (child.getText() for child in ctx.getChildren() if isinstance(child, GrammarParser.BehaviorQualifierContext))
 
-        if ctx.getChild(0, GrammarParser.BehaviorQualifierContext) is None:
-            type_ = VariableType(NamedUnqualifiedType(ctx.getChild(0, GrammarParser.TypeContext).getText()),
-                                 self.resolve_memory_qualifier(
-                                     ctx.getChild(0, GrammarParser.MemoryQualifierContext).getText()))
-        else:
-            type_ = VariableType(NamedUnqualifiedType(ctx.getChild(0, GrammarParser.TypeContext).getText()),
-                                 self.resolve_memory_qualifier(
-                                     ctx.getChild(0, GrammarParser.MemoryQualifierContext).getText()),
-                                 self.resolve_behavior_qualifier(
-                                     ctx.getChild(0, GrammarParser.BehaviorQualifierContext).getText()))
+        type_ = VariableType(
+            NamedUnqualifiedType(unqualified_type_name),
+            self.resolve_memory_qualifier(memory_qualifier_name),
+            *(self.resolve_behavior_qualifier(name) for name in behavior_qualifier_names)
+        )
 
         return VariableDeclaration(name, type_)
 
