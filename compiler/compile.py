18f577646c052511867ec2b86e1abd88332b036a
yonatan.eavri <yonatan.eavri@gmail.com>
2024-01-19 15:17:25 +0200

    Made ast printing optional

--- compiler/compile.py
+++ compiler/compile.py
@@ -1,12 +1,16 @@
 from .ast import Block, print_ast
 
+
 def compile_file(input_file_path: str, *,
-                 alternative_code: str = None, alternative_ast: Block = None,
-                 run_ir_generator: bool = True, make_executable: bool = True):
+                 alternative_code: str = None,
+                 print_generated_ast: bool = False,
+                 alternative_ast: Block = None,
+                 run_ir_generator: bool = True,
+                 make_executable: bool = True):
     """Compiles the given input file. May also replace certain compilation steps with given inputs."""
 
     if make_executable and not run_ir_generator:
-        print('Warning: recompiling without regenerating IR.')
+        print('Warning: remaking without regenerating IR.')
 
     if alternative_code is None:
         with open(input_file_path, 'r') as f:
@@ -17,7 +21,9 @@ def compile_file(input_file_path: str, *,
     if alternative_ast is None:
         from .lex_parse.create import create_ast
         tree = create_ast(code)
-        print_ast(tree)
+
+        if print_generated_ast:
+            print_ast(tree)
     else:
         tree = alternative_ast
 
