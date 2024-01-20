26c1f0603d67b362de23cfeb6406e3f1bd80df88
yonatan.eavri <yonatan.eavri@gmail.com>
2024-01-20 16:52:59 +0200

    Prepared code from compilation with binding

--- compiler/compile.py
+++ compiler/compile.py
@@ -1,4 +1,7 @@
 from .ast import Block, print_ast
+from .lex_parse.create import create_ast
+from .ir_generate.create import create_ir
+from .bind.make import make_executable
 
 
 def compile_file(input_file_path: str, *,
@@ -6,11 +9,8 @@ def compile_file(input_file_path: str, *,
                  print_generated_ast: bool = False,
                  alternative_ast: Block = None,
                  run_ir_generator: bool = True,
-                 make_executable: bool = True):
+                 make_exe: bool = True):
     """Compiles the given input file. May also replace certain compilation steps with given inputs."""
-
-    if make_executable and not run_ir_generator:
-        print('Warning: remaking without regenerating IR.')
 
     if alternative_code is None:
         with open(input_file_path, 'r') as f:
@@ -19,7 +19,6 @@ def compile_file(input_file_path: str, *,
         code = alternative_code
 
     if alternative_ast is None:
-        from .lex_parse.create import create_ast
         tree = create_ast(code)
 
         if print_generated_ast:
@@ -27,12 +26,10 @@ def compile_file(input_file_path: str, *,
     else:
         tree = alternative_ast
 
+    if make_exe and not run_ir_generator:
+        print('Warning: remaking without regenerating IR. This will result in no compilation.')
+
     if run_ir_generator:
-        from .ir_generate.create import create_ir
-        create_ir(tree)
-
-    if make_executable:
-        from .ir_generate.make import make
-        make()
-
-
+        module = create_ir(tree)
+        if make_exe:
+            make_executable(module)
