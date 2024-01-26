da61b0d95578553848a623402d5c224caa30fd3c
yonatan.eavri <yonatan.eavri@gmail.com>
2024-01-26 16:56:43 +0200

    Rewrote the evaluator

--- compiler/lex_parse/evaluator.py
+++ compiler/lex_parse/evaluator.py
@@ -1,5 +1,5 @@
 import antlr4.tree.Tree
-from antlr4 import CommonTokenStream, InputStream
+from antlr4 import CommonTokenStream, InputStream, ParserRuleContext
 
 from .grammar import GrammarLexer
 from .grammar import GrammarParser
@@ -11,168 +11,162 @@ from ..ast.types import *
 
 class GrammarASTBuilder(GrammarListener):
 
-    def add(self, ctx):
-        qualified_class_name = str(type(ctx))
-        unqualified_class_name = qualified_class_name.split('.')[-1]
-        ctx_type = unqualified_class_name[:-9]  # removes the word 'Context'
+    def add(self, ctx: ParserRuleContext) -> Any:
+        # horrific
 
-        exit_method = getattr(self, 'exit' + ctx_type, self.default_exit)
+        t_name = type(ctx).__name__
+
+        if not t_name.endswith("Context"):
+            raise TypeError(f"Unknown type {t_name} does not end with \"Context\".")
+
+        ctx_name = t_name[:-7]  # removes the word context
+
+        exit_method = getattr(self, 'exit' + ctx_name, self._error_exit)
         return exit_method(ctx)
 
-    def default_exit(self, ctx):
-        raise TypeError(f"Unknown node type: {type(ctx)}")
+    @staticmethod
+    def _error_exit(ctx):
+        raise TypeError(f"Unknown node type {type(ctx)} was not matched with a method of evaluator.")
 
-    def exitInt(self, ctx: GrammarParser.IntContext):
+    # Program:
+
+    def exitProg(self, ctx: GrammarParser.ProgContext):
+        return self.exitMultiLineBlock(ctx)
+
+    # Statement:
+
+    def exitEmptyStat(self, ctx: GrammarParser.EmptyStatContext):
+        raise TypeError(f"Attempted to create an empty ast node.")
+
+    def exitExpressionStat(self, ctx: GrammarParser.ExpressionStatContext):
+        return self.add(ctx.expr())
+
+    def exitBlockStat(self, ctx: GrammarParser.BlockStatContext):
+        return self.add(ctx.block())
+
+    # Block:
+
+    def exitMultiLineBlock(self, ctx: GrammarParser.MultiLineBlockContext):
+        return Block(tuple(self.add(s) for s in ctx.stat() if not isinstance(s, GrammarParser.EmptyStatContext)))
+
+    def exitSingleLineBlock(self, ctx: GrammarParser.SingleLineBlockContext):
+        return Block((ctx.expr(),))
+
+    # Parsing Expression:
+
+    def exitLeftSpacedExpr(self, ctx: GrammarParser.LeftSpacedExprContext):
+        return self.add(ctx.expr())
+
+    def exitRightSpacedExpr(self, ctx: GrammarParser.RightSpacedExprContext):
+        return self.add(ctx.expr())
+
+    def exitParenthesizedExpr(self, ctx: GrammarParser.ParenthesizedExprContext):
+        return self.add(ctx.expr())
+
+    # Literal Expression:
+
+    def exitIntExpr(self, ctx: GrammarParser.IntExprContext):
         return Literal(int(ctx.getText()), IntLiteralType())
 
-    def exitDouble(self, ctx: GrammarParser.DoubleContext):
+    def exitDoubleExpr(self, ctx: GrammarParser.DoubleExprContext):
         return Literal(float(ctx.getText()), DoubleLiteralType())
 
-    def exitAddSub(self, ctx: GrammarParser.AddSubContext):
-        return Operator(ctx.op.text,
-                        (self.add(ctx.getChild(0, GrammarParser.ExprContext)),
-                         self.add(ctx.getChild(1, GrammarParser.ExprContext))))
+    # Keyword Expression:
 
-    def exitMulDiv(self, ctx: GrammarParser.MulDivContext):
-        return Operator(ctx.op.text,
-                        (self.add(ctx.getChild(0, GrammarParser.ExprContext)),
-                         self.add(ctx.getChild(1, GrammarParser.ExprContext))))
+    def exitCopyExpr(self, ctx: GrammarParser.CopyExprContext):
+        return Copy(self.add(ctx.expr()))
 
-    def exitFactor(self, ctx: GrammarParser.ParenthesizeContext):
-        return self.add(ctx.getChild(0, GrammarParser.ExprContext))
+    def exitViewExpr(self, ctx: GrammarParser.ViewExprContext):
+        return View(self.add(ctx.expr()))
 
-    def exitVar(self, ctx: GrammarParser.VarContext):
-        return Variable(ctx.getText())
+    def exitReturnExpr(self, ctx: GrammarParser.ReturnExprContext):
+        subexpression = ctx.expr()
+        return ReturnVoid() if subexpression is None else Return(self.add(subexpression))
 
-    def exitAssignment(self, ctx: GrammarParser.AssignmentContext):
-        return Operator(ctx.op.text,
-                        (self.add(ctx.getChild(0, GrammarParser.ExprContext)),
-                         self.add(ctx.getChild(1, GrammarParser.ExprContext))))
+    # Operator Expression:
+
+    def exitBinaryOperatorExpr(self, ctx: GrammarParser.BinaryOperatorExprContext):
+        return Operator(ctx.op.text, (self.add(ctx.expr(0)), self.add(ctx.expr(1))))
+
+    def exitCallOperatorExpr(self, ctx: GrammarParser.CallOperatorExprContext):
+        subexpressions = tuple(self.add(e) for e in ctx.expr())
+
+        if not isinstance(subexpressions[0], Variable):
+            raise TypeError(f"Parsing error: calling a non-variable ({subexpressions[0]}).")
+
+        return Operator('()', subexpressions)
+
+    # VariableType:
 
     @staticmethod
-    def resolve_memory_qualifier(signature: str):
-        match signature:
+    def _resolve_type_name(ctx: GrammarParser.TypeNameContext):
+        return NamedUnqualifiedType(ctx.getText())
+
+    @staticmethod
+    def _resolve_memory_qualifier(ctx: GrammarParser.MemoryQualifierContext):
+        match ctx.getText():
             case '&':
                 return ValueMemoryQualifier()
 
             case '*':
                 return ReferenceMemoryQualifier()
 
-            case _:
-                raise ValueError(f'Unknown signature: "{signature}".')
+            case other:
+                raise ValueError(f'Unknown signature: "{other}".')
 
     @staticmethod
-    def resolve_behavior_qualifier(signature: str):
-        match signature:
+    def _resolve_behavior_qualifier(ctx: GrammarParser.BehaviorQualifierContext):
+        match ctx.getText():
             case 'noread':
                 return NoReadBehaviorQualifier()
 
             case 'nowrite':
                 return NoWriteBehaviorQualifier()
 
-            case _:
-                raise ValueError(f'Unknown signature: "{signature}".')
+            case other:
+                raise ValueError(f'Unknown signature: "{other}".')
 
-    def exitDeclaration(self, ctx: GrammarParser.DeclarationContext):
-
-        *_, name = ctx.getChildren()
-        name = name.getText()
-
-        unqualified_type_name = ctx.getChild(0, GrammarParser.TypeContext).getText()
-        memory_qualifier_name = ctx.getChild(0, GrammarParser.MemoryQualifierContext).getText()
-        behavior_qualifier_names = (child.getText() for child in ctx.getChildren() if
-                                    isinstance(child, GrammarParser.BehaviorQualifierContext))
-
-        type_ = VariableType(
-            NamedUnqualifiedType(unqualified_type_name),
-            self.resolve_memory_qualifier(memory_qualifier_name),
-            tuple(self.resolve_behavior_qualifier(name) for name in behavior_qualifier_names)
+    def exitFullVariableType(self, ctx: GrammarParser.FullVariableTypeContext):
+        return VariableType(
+            self._resolve_type_name(ctx.typeName()),
+            self._resolve_memory_qualifier(ctx.memoryQualifier()),
+            tuple(self._resolve_behavior_qualifier(q) for q in ctx.behaviorQualifier())
         )
 
-        return VariableDeclaration(name, type_)
-
-    def exitEmptyLine(self, ctx: GrammarParser.EmptyLineContext):
-        return None  # No AST node for empty lines
-
-    def exitExpressionLine(self, ctx: GrammarParser.ExpressionLineContext):
-        return self.add(ctx.getChild(0, GrammarParser.ExprContext))  # Build AST for the expression
-
-    def exitParenthesize(self, ctx: GrammarParser.ParenthesizeContext):
-        return self.add(ctx.getChild(1))
-
-    def exitCopy(self, ctx: GrammarParser.CopyContext):
-        return Copy(self.add(ctx.getChild(0, GrammarParser.ExprContext)))
-
-    def exitView(self, ctx: GrammarParser.ViewContext):
-        return View(self.add(ctx.getChild(0, GrammarParser.ExprContext)))
-
-    def exitReturn(self, ctx: GrammarParser.ReturnContext):
-        return Return(self.add(ctx.getChild(0, GrammarParser.ExprContext)))
-
-    def exitEmptyReturn(self, ctx: GrammarParser.EmptyReturnContext):
-        return ReturnVoid()
-
-    def exitFuncLiteral(self, ctx: GrammarParser.FuncLiteralContext):
-        parameters = []
-
-        # combine the 3 loops into one, it's not hard
-        if isinstance(ctx.getChild(0), GrammarParser.TypeContext):  # No behavior
-            for i, child in enumerate(ctx.getChildren(GrammarParser.ExprContext)):
-                if not isinstance(child, antlr4.tree.Tree.TerminalNodeImpl) and not isinstance(child,
-                                                                                               GrammarParser.BlockContext) and i > 2:
-                    if not isinstance(child, GrammarParser.DeclarationContext):
-                        raise TypeError("Parsing error: Unfitting expression when trying to parse a function parameter")
-                    parameters.append(self.add(child))
-
-            return Function.make(
-                VariableType(NamedUnqualifiedType(ctx.getChild(0, GrammarParser.TypeContext).getText()),
-                             self.resolve_memory_qualifier(
-                                 ctx.getChild(0, GrammarParser.MemoryQualifierContext).getText()), ()),
-                parameters, self.add(ctx.getChild(0, GrammarParser.BlockContext)))
-
-        elif isinstance(ctx.getChild(1), GrammarParser.TypeContext):  # Yes behavior
-            for i, child in enumerate(ctx.getChildren(GrammarParser.ExprContext)):
-                if not isinstance(child, antlr4.tree.Tree.TerminalNodeImpl) and not isinstance(child,
-                                                                                               GrammarParser.BlockContext) and i > 3:
-                    if not isinstance(child, GrammarParser.DeclarationContext):
-                        raise TypeError("Parsing error: Unfitting expression when trying to parse a function parameter")
-                    parameters.append(self.add(child))
-
-            return Function.make(
-                VariableType(NamedUnqualifiedType(ctx.getChild(0, GrammarParser.TypeContext).getText()),
-                             self.resolve_memory_qualifier(
-                                 ctx.getChild(0, GrammarParser.MemoryQualifierContext).getText()), ()),
-                # add behavior qualifier here
-                parameters, self.add(ctx.getChild(0, GrammarParser.BlockContext)))
-
-        for i, child in enumerate(ctx.getChildren(GrammarParser.ExprContext)):
-            if not isinstance(child, antlr4.tree.Tree.TerminalNodeImpl) and not isinstance(child,
-                                                                                           GrammarParser.BlockContext):
-                if not isinstance(child, GrammarParser.DeclarationContext):
-                    raise TypeError("Parsing error: Unfitting expression when trying to parse a function parameter")
-                parameters.append(self.add(child))
-
-        return Function.make(VoidType(), parameters, self.add(ctx.getChild(0, GrammarParser.BlockContext)))
-
-    def exitFuncCall(self, ctx: GrammarParser.FuncCallContext):
-        children = list(self.add(c) for c in ctx.getChildren() if
-                        not isinstance(c, (GrammarParser.EmptyLineContext, antlr4.tree.Tree.TerminalNodeImpl)))
-        children.insert(0, Variable(ctx.getChild(0).getText()))
-        if not isinstance(children[0], Variable):
-            raise TypeError(f"Parsing error: calling a non-variable ({children[0]}).")
-
-        return Operator(
-            '()',
-            children
+    def exitAutoVariableType(self, ctx: GrammarParser.AutoVariableTypeContext):
+        return VariableType(
+            AutoUnqualifiedType(),
+            self._resolve_memory_qualifier(ctx.memoryQualifier()),
+            tuple(self._resolve_behavior_qualifier(q) for q in ctx.behaviorQualifier())
         )
 
-    def exitBlock(self, ctx: GrammarParser.BlockContext):
-        return Block(tuple(self.add(c) for c in ctx.getChildren() if
-                           not isinstance(c, GrammarParser.EmptyLineContext) and not isinstance(c,
-                                                                                                antlr4.tree.Tree.TerminalNodeImpl)))
+    # Variable Expression:
 
-    def exitProg(self, ctx: GrammarParser.ProgContext):
-        return self.exitBlock(ctx)
+    def exitVarExpr(self, ctx: GrammarParser.VarExprContext):
+        return Variable(ctx.name.text)
+
+    def exitVariableDeclarationExpr(self, ctx: GrammarParser.VariableDeclarationExprContext):
+        return VariableDeclaration(ctx.name.text, self.add(ctx.variableType()))
+
+    # Value Creation
+
+    def exitFunctionCreationExpr(self, ctx: GrammarParser.FunctionCreationExprContext):
+        pass
+
+    def exitVoidReturningFunctionCreationExpr(self, ctx: GrammarParser.VoidReturningFunctionCreationExprContext):
+
+        parameters = tuple(self.add(e) for e in ctx.expr())
+
+        if any(not isinstance(p, VariableDeclaration) for p in parameters):
+            raise TypeError(f'Attempted to use a non-variable-declaration as a parameter.')
+
+        return Function.make(
+            VoidType(),
+            parameters,
+            ctx.block()
+        )
+
+    # builder:
 
     def build_ast(self, input_string):
         lexer = GrammarLexer(InputStream(input_string))
