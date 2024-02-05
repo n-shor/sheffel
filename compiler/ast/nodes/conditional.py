1c12ddbab2d23b962ee0083c5cc9159fc0466f8c
yonatan.eavri <yonatan.eavri@gmail.com>
2024-02-05 19:06:55 +0200

    Separated the conditional into with/without else clause

--- compiler/ast/nodes/conditional.py
+++ compiler/ast/nodes/conditional.py
@@ -4,9 +4,15 @@ from . import Node
 
 
 @dataclass
-class Conditional(Node):
+class IfConditional(Node):
     """Represents a conditional if statement."""
 
     condition: Node
     on_true: Node
+
+
+@dataclass
+class IfElseConditional(IfConditional):
+    """Represents a conditional if statement which includes an else clause."""
+
     on_false: Node
