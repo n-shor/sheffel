e68711d1787244c66172917e63a1363a24efc9d7
yonatan.eavri <yonatan.eavri@gmail.com>
2024-02-10 11:26:53 +0200

    Simplified compilation process to use only clang

--- compiler/build/create.py
+++ compiler/build/create.py
@@ -3,52 +3,22 @@ import os
 
 from llvmlite import ir, binding
 
-from .file_constants import *
+
+BUILD_DIR = "build_files"
 
 
-def create_exe(module: ir.Module):
-    module.name = "module"
+def create_exe(module: ir.Module, name: str = 'Program'):
+    module.name = name
     module.triple = "x86_64-unknown-linux-gnu"
     module.data_layout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
 
-    if not os.path.isdir(BUILD_PATH):
-        os.makedirs(BUILD_PATH)
+    if not os.path.isdir(BUILD_DIR):
+        os.makedirs(BUILD_DIR)
 
-    with open(f"{BUILD_PATH}/{IR_FILE}", 'w') as f:
+    ir_file_path = f'"{BUILD_DIR}/{name}.ll"'
+    exe_file_path = f'"{BUILD_DIR}/{name}.exe"'
+
+    with open(ir_file_path, 'w') as f:
         f.write(str(module))
 
-    result = -1
-
-    try:
-        result = _mingw64_compilation_step(
-            'llc',
-            f'"{BUILD_PATH}/{IR_FILE}"',
-            '-o', f'"{BUILD_PATH}/{ASM_FILE}"',
-            name="Assembly file generation")
-
-        result = _mingw64_compilation_step(
-            'gcc',
-            '-c', f'"{BUILD_PATH}/{ASM_FILE}"',
-            '-o', f'"{BUILD_PATH}/{OBJECT_FILE}"',
-            name="Object file generation")
-
-        result = _mingw64_compilation_step(
-            'gcc',
-            f'"{BUILD_PATH}/{OBJECT_FILE}"',
-            '-o', f'"{BUILD_PATH}/{EXECUTABLE_FILE}"',
-            name="Executable file generation")
-
-    except RuntimeError:
-        return result
-
-    return result
-
-
-def _mingw64_compilation_step(*args: str, name: str):
-    result = subprocess.call(['mingw64', *args])
-
-    if result != 0:
-        raise RuntimeError(f'{name} process returned with a non-zero result {result}.')
-
-    print(f'{name} successful.')
-    return result
+    return subprocess.call(['clang', ir_file_path, '-o', exe_file_path])
--- compiler/build/file_constants.py
+++
@@ -1,6 +0,0 @@
-BUILD_PATH = './build_files'
-
-IR_FILE = 'generated_ir.ll'
-ASM_FILE = 'generated_assembly.s'
-OBJECT_FILE = 'generated_object.o'
-EXECUTABLE_FILE = 'generated_executable.exe'
