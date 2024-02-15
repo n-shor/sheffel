46010f4160cf895904f26c0927c2ba15d2f024e3
nadva <nbm1401@gmail.com>
2024-02-15 12:51:48 +0200

    Added examples folder and used code reading from file

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-from compiler.compile import compile_from_code
+from compiler.compile import compile_from_file
 
 
 def main():
 
-    code = """
-    Int& x = 5
-    Int& y = 0
-    
-    while y < 5
-    {
-        x = x + 1
-        y = y + 1
-    }
-
-    return x
-    """
-
-    compile_from_code(code, regenerate_grammar=False, print_ir=True, print_result=True)
+    compile_from_file('examples/primes.shf', regenerate_grammar=False, print_ir=True, print_result=True)
 
 
 if __name__ == "__main__":
