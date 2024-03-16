83e9753d3c11a6f0cc75c97b75141a3b680104b7
yonatan.eavri <yonatan.eavri@gmail.com>
2024-03-16 17:38:28 +0200

    Standardised the chaining of compilation steps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from compiler.structure import grammar, pure
-from compiler.translate import evaluator
+from compiler.structure import grammar, pure, reductive
+from compiler.translate import Translator, Evaluator
 
 code = """
 return 3
@@ -9,14 +9,23 @@ xd& a = 8 - 2 * 2
 """
 
 
+class Compiler(Translator[str, pure.Node]):
+    def __init__(self):
+        self.translators = (Evaluator(), )
+
+    def translate(self, source):
+        step = source
+        for translator in self.translators:
+            step = translator.translate(step)
+        return step
+
+
 def main():
     grammar.utils.regenerate()
 
-    print(
-        evaluator.Evaluator()
-        .translate(code)
-        .hierarchy()
-    )
+    result = Compiler().translate(code)
+
+    print(result.hierarchy())
 
 
 if __name__ == "__main__":
