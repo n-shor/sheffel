2f88f57766141100143e311cdbba8ff3f1302736
yonatan.eavri <yonatan.eavri@gmail.com>
2024-04-09 17:44:39 +0300

    Added a result value to be returned from operators

--- compiler/structure/resolved/values/__init__.py
+++ compiler/structure/resolved/values/__init__.py
@@ -1,1 +1,2 @@
 from .literal_value import LiteralValue
+from .result_value import ResultValue
--- compiler/structure/resolved/values/literal_value.py
+++ compiler/structure/resolved/values/literal_value.py
@@ -4,7 +4,7 @@ from .. import Value, Type
 
 
 class LiteralValue(Value):
-    def __init__(self, type_: Type, py_value, ir_value: ir.Value):
+    def __init__(self, type_: Type, py_value, ir_value: ir.Constant):
         super().__init__(type_)
         self._py_value = py_value
         self._ir_value = ir_value
